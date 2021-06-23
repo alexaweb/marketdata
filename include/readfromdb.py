e8fcc1bb34d2d2b2c3ce078ef8ee2e90e29f3739
Unknown <alexander.celle@gmail.com>
2021-06-23 18:18:29 -0400

    read

--- include/readfromdb.py
+++ include/readfromdb.py
@@ -13,3 +13,18 @@ def writePriceToDb(string,bucket,org,url,token):
     client = influxdb_client.InfluxDBClient(url=url, token=token, org=org)
     write_api = client.write_api(write_options=SYNCHRONOUS)
     write_api.write(bucket=bucket, org=org, record=string)
+
+
+def readFromDB(string,bucket,org,url,token):
+    client = influxdb_client.InfluxDBClient(url=url, token=token, org=org) 
+    query_api = client.query_api()
+    query = 'from(bucket:bucket)\
+    |> range(start: -10d)\
+    |> tiler(fn(r) => r.ticker = string)'
+    ## Using Table Structure
+    result = query_api.query(org=org, query=query)
+    results = []
+
+    for table in result:
+        for record in table.records:
+            print (record.values)
