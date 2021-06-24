18545a8b217c2b946ae94f1d0eef761954cb1a1c
Unknown <alexander.celle@gmail.com>
2021-06-23 21:23:19 -0400

    dataframe

--- readtickerfromDB.py
+++ readtickerfromDB.py
@@ -8,6 +8,7 @@ import argparse
 
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
+from gspread_dataframe import get_as_dataframe, set_with_dataframe
 
 # Third party imports
 
